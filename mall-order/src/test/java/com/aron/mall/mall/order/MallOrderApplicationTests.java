06cb92a48b8ff38f1009859ea9970f21ccd11b2b
zhaochunlong <1960528539@qq.com>
2022-12-14 10:42:38 +0800

    三高项目第一次提交，项目初始化

---
+++ mall-order/src/test/java/com/aron/mall/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aron.mall.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
